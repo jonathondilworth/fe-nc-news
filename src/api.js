220cfa180683beece1f5f0cd30635d5a35350bec
Jon Dilworth <jon@dilworth.dev>
2024-03-11 15:39:06 +0000

    removed additional features

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const ncNewsApi = axios.create({ baseURL: 'https://nc-news-1fcj.onrender.com/api' });
 
-export const getArticles = (topic, sortBy, orderBy) => {
-    return ncNewsApi.get('/articles', { params: { topic: topic, sort_by: sortBy, order: orderBy }})
+export const getArticles = () => {
+    return ncNewsApi.get('/articles')
     .then(({ data: { articles } }) => {
         return articles;
     });
